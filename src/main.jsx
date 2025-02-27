53890eaabec15d90dc3667952a02333a932b773a
jessicalima22 <jessica.lima22@hotmail.com>
2025-02-26 22:28:34 -0300

    testing 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div style={{ padding: '20px', color: 'white' }}>
     <h2>This is a direct test</h2>
-    <p>If you can see this text, React is working!</p>
+    <p>If you can see this text, React is  working!</p>
   </div>
 )
