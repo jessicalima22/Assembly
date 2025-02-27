26e4d4b64a321eda8cad7d758d620b980c9e45e1
jessicalima22 <jessica.lima22@hotmail.com>
2025-02-26 21:21:25 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+import React from "react"
+import './App.css'
+
+export default function AssemblyEndgame() {
+  return (
+      <main>
+          <header>
+              <h1>Assembly: Endgame</h1>
+              <p>Guess the word within 8 attempts to keep the 
+              programming world safe from Assembly!</p>
+          </header>
+      </main>
+  )
+}
+
